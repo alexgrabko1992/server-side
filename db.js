0d053e10a08701586bc1cf8ba72cf435ea7d36a3
Alex <alexgrabko1992@gmail.com>
2022-02-21 20:50:16 +0300

    initial

--- db.js
+++ db.js
@@ -2,23 +2,18 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 module.exports = new Sequelize(
-  "d9mre3c2e9qefj",
-  "csuerzkmpnetfp",
-  "efac5a66eb348cade26cc55ffee1ad921fb85b033313232b8fb7cf3d6ba8b6fd",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     dialect: "postgres",
-    port: 5432,
-    host: "ec2-44-195-186-235.compute-1.amazonaws.com",
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
   }
 );
 
-// module.exports = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     dialect: "postgres",
-//     port: process.env.DB_PORT,
-//     host: process.env.DB_HOST,
-//   }
-// );
+// DB_NAME = recom4u;
+// DB_USER = postgres;
+// DB_PASSWORD = postgres;
+// DB_HOST = localhost;
+// DB_PORT = 5432;
